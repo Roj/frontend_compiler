7eed4bc831d2bdaf0a879a2a29147bbe99fa21db
joaquintz <joa.dev@live.com>
2018-03-06 15:54:23 -0300

    Template for test suite.

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
-CFLAGS=-std=c99 -O1 -pedantic -Werror -Wtype -Wextra -Wall
+CLIBS=-lcheck -lsubunit -pthread -pthread -lrt -lm -lsubunit
+CFLAGS=-std=c99 -O1 -pedantic -Werror -Wextra -Wall  
 CC=gcc
 
 main: lexical.o
-	$(CC) $(CFLAGS) lexical.o -o main
+	$(CC) $(CFLAGS) lexical.o $(CLIBS) -o main
 
+clean:
+	rm -f *.o main
 
+.PHONY: clean
--- lexical.c
+++ lexical.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdint.h>
+#include <check.h>
+
 typedef enum _lexeme_type_t { 
 	NUMBER,
 	IDENTIFIER,
@@ -22,8 +25,27 @@ typedef struct lexeme {
 	} data;
 } lexeme_t;
 
-int main() { 
-	char* input = "1 + 2";
+START_TEST (lexeme_string_test) {
+	ck_assert_int_eq(3,3);
+}
+END_TEST
 
-	return 0;
+Suite* lexical_suite(void) {
+	Suite* s;
+	TCase* tc_core;
+	s = suite_create("Lexical analyzer");
+	tc_core = tcase_create("Core");
+	tcase_add_test(tc_core, lexeme_string_test);
+	suite_add_tcase(s, tc_core);
+	return s;
+}
+
+int main() { 
+	//char* input = "1 + 2";
+	Suite* s = lexical_suite();
+	SRunner* sr = srunner_create(s);
+	srunner_run_all(sr, CK_NORMAL);
+	int number_failed = srunner_ntests_failed(sr);
+	srunner_free(sr);
+	return (number_failed == 0)? EXIT_SUCCESS : EXIT_FAILURE;
 }
