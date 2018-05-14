63425d29e020a6416e0e323be71b9b147f0cb9d9
joaquintz <joa.dev@live.com>
2018-05-15 00:45:21 +0200

    Parser front-end can handle files (multiline input)

--- parser.c
+++ parser.c
@@ -2,14 +2,27 @@
 #include "libs/parser.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
 
-int main() {
-	char line[100];
-	printf("Syntactical parser -- write code one block per line up to 100 "
+//Assumption: first line is not empty. (sorry)
+int main(int argc, char* argv[]) {
+	char line[3000];
+	if (argc == 3 && strcmp(argv[1], "--file") == 0) {
+		printf("Parsing file %s\n", argv[2]);
+		lexeme_t* start = process_file(argv[2]);
+		if (parse(start)) {
+			printf("Parsed correctly!\n");
+		} else {
+			printf("Does not fit the syntax!\n");
+		}
+		return 0;
+	}
+
+	printf("Syntactical parser -- write code one block per line up to 3000 "
 		"characters long\n");
 
 	bool unfinished_comment = false;
-	while (fgets(line, 99, stdin) != NULL) {
+	while (fgets(line, 2999, stdin) != NULL) {
 		lexeme_t* start = process_string(line, &unfinished_comment);
 
 		lexeme_t* current = start;
